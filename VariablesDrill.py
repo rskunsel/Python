9cc4b55ef84532000bdd3ba47298bb90b879efca
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-10-31 02:38:37 -0500

    Add files via upload

---
+++ VariablesDrill.py
@@ -0,0 +1,92 @@
+'''
+integers 
+'''
+
+var1 = 5
+print(var1) 
+
+var2 = 10 
+print(var2)
+
+x = 24 
+print(x)
+
+y =100 
+print(y)
+
+'''
+Float
+'''
+z = 1.1 
+print(z)
+
+a = 2.75 
+print (a)
+
+b = a 
+print(b)
+
+c = 100.1
+print(c)
+
+d = 12.3
+print(d)
+
+'''
+strings 
+'''
+name = "rinzin"
+print(name)
+
+smart = "true"
+print(smart)
+
+i = "done"
+print(i)
+
+j= "false"
+print(j)
+
+almostDone = "true" 
+print (almostDone)
+
+'''
+Boleans
+'''
+Done = False 
+print(Done)
+
+Smart = True
+print(Smart)
+
+i = Done 
+print(i)
+
+j = False 
+print(j)
+
+almostDone = True 
+print(almostDone)
+
+'''
+Lists
+'''
+
+listInt = [1, 2, 3, 4, 5,]
+print[listInt(3)]
+
+listString = [1.1, 2.2, 3.3, 4.4, 5.5]
+print[listString(4.4)]
+
+listBoolean = [True, False, False, True, False]
+print [listBoolean(True,False,False,True,False)]
+
+
+
+
+
+
+
+
+
+
