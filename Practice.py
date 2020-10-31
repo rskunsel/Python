c85bd72366ed628fd853168c1a7c39583c43d436
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-10-31 01:45:24 -0500

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,94 @@
+'''
+
+{name} = {data}
+
+
+'''
+
+#integer
+ 
+a = 5
+a = 4 
+
+#float 
+
+a = 3.45 
+a = 2.1 
+
+#string 
+a = "string"
+name = "Rinzin " and "Michael"
+
+#Boolean 
+
+a = True 
+a = False
+
+#list 
+
+a = {3, 4, 5, 6}
+
+a = {"stringOne","stringTwo"}
+a = {True, False, True}
+a = {1.1, 2.2, 3.3}
+
+
+
+
+varA = 1 
+vara = 2
+
+print (vara)
+print(a) 
+
+
+var = 4
+var = "string"
+varTwo = var
+print(varTwo)
+
+
+b = [a, varA, var]
+
+list = ["a", "b", "c"]
+
+print(list)
+print(list[0])
+
+listVar = 1 
+print (listVar)
+
+
+string = "rinzin"
+
+print(string [0])
+
+print(5)
+print("string")
+print(True)
+print (list)
+print(string)
+
+'''
+SyntaxError
+
+Logical 
+'''
+
+
+
+var = 4 
+name = 4
+
+name = "rinzin"
+
+varThree = 0 
+var = varThree
+
+rinzin = "name"
+
+print (rinzin)
+
+titles = "rinzin"
+
+print (titles)
