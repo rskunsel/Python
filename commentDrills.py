048062a625c2dda367c05138b807ed06fb0f9e02
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-10-31 03:05:28 -0500

    Add files via upload

---
+++ commentDrills.py
@@ -0,0 +1,23 @@
+'''
+Created on Oct 31, 2020
+
+@author: ITAUser
+'''
+#single line comment
+
+
+'''
+multi lane comments
+al;sdhf;iwahg;lkdsaj
+asdjfah;sdfjsad;UnicodeNFKCFileTestsasd
+
+'''
+var = 6 #comment after it 
+ #single line comment 
+ 
+ '''
+ laksjdg;alsdkjfas;lkdfj
+ multi lane comment 
+ aidsjfk;asldkfjas;AsyncYieldFromas
+ 
+ '''
