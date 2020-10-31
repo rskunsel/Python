ad1db5db669de35657d504d554dbd2cfbf0acf87
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-10-31 02:01:35 -0500

    Add files via upload

---
+++ VariablesOutline.py
@@ -0,0 +1,111 @@
+'''
+
+{name} = {data}
+
+
+'''
+
+#integer
+
+a=8
+ 
+a = 5
+a = 4 
+
+#float 
+a=8.8
+
+a = 3.45 
+a = 2.1 
+
+#string 
+a = "string"
+name = "Rinzin " and "Michael"
+
+a= "string"
+name = "Tenzin"
+#Boolean 
+
+a = True 
+a = False
+
+a = True 
+a = False 
+#list 
+
+a = {3, 4, 5, 6}
+
+a = {"stringOne","stringTwo"}
+a = {True, False, True}
+a = {1.1, 2.2, 3.3}
+
+a = {8, 7, 6, 5,}
+
+a = {"stringThree", "stringFour"}
+a = {True, False, False}
+
+varA = 1 
+vara = 2
+
+print (vara)
+print(a) 
+
+varA = 4
+vara =3
+
+varA = vara
+
+var = 4
+var = "string"
+varTwo = var
+print(varTwo)
+
+a = (a, varA, vara)
+b = [a, varA, var]
+
+list = ["a", "b", "c"]
+
+print(list)
+print(list[0])
+
+listVar = 1 
+print (listVar)
+
+
+string = "rinzin"
+print(a)
+
+print(string [0])
+
+print(5)
+print("string")
+print(True)
+print (list)
+print(string)
+
+'''
+SyntaxError
+
+Logical 
+'''
+
+
+
+var = 4 
+name = 4
+
+name = "rinzin"
+
+varThree = 0 
+var = varThree
+
+rinzin = "name"
+
+print (rinzin)
+
+titles = "rinzin"
+
+print (titles) 
+
+titles = "rinzin"
+print (title)
