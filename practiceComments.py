9f2cc79ede9de1c9f4c5f564e40de7380185a45f
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-10-31 02:57:36 -0500

    Add files via upload

---
+++ practiceComments.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 31, 2020
+
+@author: ITAUser
+'''
+
+#a;isdhf;iawhgjksvdalkjsdfhaof
+
+'''
+a/sdkgja'sdkllf;lad
+a/s;odjga'fsdljf;k;lADSf
+a;lsdjhf;awjdkfksa
+alhfdawhg;alksdvjsk;a
+aksjdfhawhladsfj;fn
+fawrljsfdl;asdkf'la
+
+
+'''
+#This isn't working, can't debug, what do you think? 
+
+var = 4 #single line comment
+print (var)
