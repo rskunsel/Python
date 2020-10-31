e22d0fbc3725d9c75d9dfb1f852543cd110995cb
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-10-31 03:21:08 -0500

    Add files via upload

---
+++ debugInputdrill.py
@@ -0,0 +1,18 @@
+'''
+pi = 3.14 
+
+circle area () 
+
+'''
+function calculates the area of a rectangle 
+#comment section, forgot to out something in the parentehsis above 
+'''
+'''
+width = input  ()
+#didn't put an input in the ()
+'''
+'''
+def rectangleArea()
+#didn't add anything in ()
+
+'''
