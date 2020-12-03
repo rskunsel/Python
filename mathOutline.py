f97bd08a1bc74ac4f1cf8e0a7b9acad4be73db16
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-12-03 00:20:41 -0600

    Add files via upload

---
+++ mathOutline.py
@@ -0,0 +1,97 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+
+var = 4 + 4 
+x = 4+4
+print(x)
+
+y = "name" + "your"
+print (y) 
+
+x = 4 - 3
+print (x) 
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+x = 4 - 3 
+x = 4 * 4 
+print(x)
+
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+x = "michael" * 4
+print (x) 
+x = 4/4 
+print (x) 
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+
+x = 4/4 
+print (x)
+
+var = 20 
+varTwo = 2 
+
+x = var * varTwo
+print (x)
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+x = 5 % 4 
+print (x)
+
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+
+x = 4 // 4 
+print (x)
+
+x = 5/4 
+print (x) 
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+
+x = 4 // 4 
+printe (x) 
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+x = 2 ** 2 
+print (x) 
+
+x = 4 ** 2 
+print (x) 
+
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+
+
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+x = 4 // 4 
+print (x) 
+
+print ("This is" + string)
+
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+y = 2 ** 2 
+print (y)
+
+
+#12) Make any type of operation with a common SYNTAX error
+
+printe (x) 
+x = 4 % 5 
