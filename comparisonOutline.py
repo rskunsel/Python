0829086e22e246ec9de55acd8b03c401c150012c
rskunsel <71422533+rskunsel@users.noreply.github.com>
2020-12-02 23:40:29 -0600

    Add files via upload

---
+++ comparisonOutline.py
@@ -0,0 +1,70 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+'''
+4 == 4 
+#True 
+
+2==4 
+#False 
+
+2< 3
+#true
+
+3<2
+#False 
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+'''
+v = (4==5) 
+print (v) 
+
+
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+
+x = "michael" == "Tenzin"
+print (x) 
+
+x = "michael"=="Michael 
+print (x) 
+
+x = "michael" == "michael" 
+print (x) 
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+ 
+ x = "michael" == "michael" 
+print (x) 
+
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+
+y = 4 > = 4
+#True 
+print (y) 
+
+r = 3 > = 4 
+print (r) 
+#flase
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+v = (4 == 5) 
+print (v) 
+
+g = "rinzin" == "rinzin "
+print (g) 
+#True 
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+
+x = "michael" == "Michael"
+# False because one M is lower case 
